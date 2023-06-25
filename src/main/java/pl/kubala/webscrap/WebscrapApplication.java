a4ddd03f6e0d69b9b8de62055cc783b1aac041c6
kubala2908 <j.b.tuminski@gmail.com>
2023-06-25 19:03:49 +0200

    initial commit

---
+++ src/main/java/pl/kubala/webscrap/WebscrapApplication.java
@@ -0,0 +1,13 @@
+package pl.kubala.webscrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebscrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebscrapApplication.class, args);
+	}
+
+}
